6eda928eddcdf9973bf56d5feebc1709db2f953f
OpenArcher34166 <svaldinei743@gmail.com>
2026-06-19 23:58:07 -0300

    debug  discord login

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,24 +1,23 @@
-const atualizarPainel = require("../utils/atualizarPainel");
-
 module.exports = (client) => {
-  client.once("ready", async () => {
+
+  client.once("clientReady", async () => {
+
     try {
 
+      console.log("=================================");
       console.log(`🤖 Bot online como ${client.user.tag}`);
-
-      console.log("READY EXECUTADO");
-
-      // NÃO BUSCA MAIS CANAL FIXO
-      // const canal = await client.channels.fetch("1515543183688208404");
-
-      // NÃO ATUALIZA PAINEL POR ENQUANTO
-      // await atualizarPainel(client);
-
-      console.log("BOT PRONTO");
+      console.log("✅ READY EXECUTADO");
+      console.log(`🆔 BOT ID: ${client.user.id}`);
+      console.log(`📡 Servidores: ${client.guilds.cache.size}`);
+      console.log("=================================");
 
     } catch (err) {
-      console.error("ERRO READY:");
+
+      console.error("❌ ERRO READY:");
       console.error(err);
+
     }
+
   });
+
 };
